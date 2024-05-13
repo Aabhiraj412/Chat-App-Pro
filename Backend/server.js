035706b7621bbc5d0068f2d405f7cf7c0af2da59
Abhiraj <aabhiraj412@gmail.com>
2024-05-13 17:37:49 +0530

    Backend 70%

---
+++ Backend/server.js
@@ -0,0 +1,34 @@
+// Modules
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+// Routes
+import authRouter from './Routes/auth.routes.js';
+import messageRouter from './Routes/message.routes.js';
+import userRouter from './Routes/user.routes.js';
+
+// DataBase
+import connecttodb from './DB/connect.js';
+
+
+dotenv.config();
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+// app.post('/', (req,res)=>{
+//     res.send("Abhiraj is Great");
+// });
+
+app.use(express.json())
+app.use(cookieParser());
+
+app.use('/api/auth', authRouter);
+app.use('/api/message', messageRouter);
+app.use('/api/user', userRouter);
+
+app.listen(PORT,()=>{
+    connecttodb()
+    console.log(`Server Listening at port https://localhost:${PORT}`);
+});
